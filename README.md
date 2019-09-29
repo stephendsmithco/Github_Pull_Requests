f21456feb37390094d19de7e76f104563a5d2988
Stephen Smith <stephendsmithco@gmail.com>
2019-09-29 12:01:32 -0600

    logging, swagger, better formatting

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# NodeJSExpressTemplate
-This is meant as a basic template to start a NodeJS server.  Currently uses StandardJS for a linter.
+# Github_Pull_Requests
+This is an app that displays pull request information for a given public github repository.
+Swagger documentation for the back-end is available at /api-docs
+
+# Dev Server start up
+npm i
+npm run start:dev  // This starts in dev mode, which will automatically restart the server when changes are detected
--- src/controllers/gh_pull_requests_controller.js
+++ src/controllers/gh_pull_requests_controller.js
@@ -1,41 +1,62 @@
-'use strict'
-const rpNative = require('request-promise-native')
+const helper = require('../helpers/gh_request_helper')
+const { getUsernameRepoFromUrl, getGHResponseFromUrl } = helper
 
 const GITHUB_BASE_URL = 'https://api.github.com'
 
-async function getGHResponseFromUrl (url) {
-  return JSON.parse(await rpNative.get(url,
-    {
-      headers: {
-        'User-Agent': 'stephendsmithco'
-      }
-    }))
-}
-
 async function getGHPullRequests (req, res) {
-  const githubUrl = req.query.github_url
-  const urlSplit = githubUrl.split('/')
-  const username = urlSplit[3]
-  const repo = urlSplit[4]
-  const response = await getGHResponseFromUrl(`${GITHUB_BASE_URL}/repos/${username}/${repo}/pulls`)
+  // Parse information from the gh_repository_url to get username and repo name
+  let username
+  let repo
+  try {
+    const githubInfo = getUsernameRepoFromUrl(req.query.gh_repository_url)
+    username = githubInfo.username
+    repo = githubInfo.repo
+  } catch (err) {
+    console.log(err)
+    // If there is an error at this stage, it is a bad request
+    res.status(400).send(err)
+    return
+  }
+
+  // Get general information about the repo
+  let response
+  try {
+    response = await getGHResponseFromUrl(`${GITHUB_BASE_URL}/repos/${username}/${repo}/pulls`, username)
+  } catch (err) {
+    res.status(500).send(err)
+    return
+  }
 
   const numPullRequests = response.length
+
+  // In order to grab commits and comments about the pull requests, we will need to call separate endpoints, this array will hold the promises so they can be done async
   const detailsPromisesArray = []
 
+  // Parse information about pull requests and set up calls to get comments and commits count
   const results = response.reduce((pullRequestInfos, pullRequest) => {
     pullRequestInfos.push({
       html_url: pullRequest.html_url,
       title: pullRequest.title
     })
-    detailsPromisesArray.push(getGHResponseFromUrl(pullRequest.comments_url))
-    detailsPromisesArray.push(getGHResponseFromUrl(pullRequest.commits_url))
+    detailsPromisesArray.push(getGHResponseFromUrl(pullRequest.comments_url, username))
+    detailsPromisesArray.push(getGHResponseFromUrl(pullRequest.commits_url, username))
     return pullRequestInfos
   }, [])
-  const detailsResponses = await Promise.all(detailsPromisesArray)
+
+  let detailsResponses
+  try {
+    detailsResponses = await Promise.all(detailsPromisesArray)
+  } catch (err) {
+    res.status(500).send(err)
+    return
+  }
+
+  // Each pr will have two corresponding promises (one for commits, one for comments)
   for (let i = 0; i < numPullRequests; i++) {
-    results[i].comments_count = detailsResponses[i * 2].length
+    results[i].comment_count = detailsResponses[i * 2].length
     results[i].commit_count = detailsResponses[(i * 2) + 1].length
   }
+
   res.send({
     count: numPullRequests,
     results: results
